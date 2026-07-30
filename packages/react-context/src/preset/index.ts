e119b88e620159efc462f2ce7108e91c72761207
SokuRitszZ <1062708758@qq.com>
2026-07-31 02:25:11 +0800

    feat: add custom state in model

---
+++ packages/react-context/src/preset/index.ts
@@ -0,0 +1,2 @@
+export * from './model-state-local-storage';
+export * from './model-state-url-query';
---
+++ packages/react-context/src/preset/model-state-url-query.ts
@@ -0,0 +1,27 @@
+import { DefineCreateModelCtxSyncState } from '../define';
+
+export interface CreateModelCtxPresetSyncStateUrlOption {
+  Key: string;
+}
+
+export const CreateModelCtxPresetSyncStateUrl = DefineCreateModelCtxSyncState((options: CreateModelCtxPresetSyncStateUrlOption) => {
+  return {
+    Load: () => {
+      const params = new URLSearchParams(location.search);
+      const jsonString = params.get(options.Key) || '';
+      try {
+        return JSON.parse(jsonString);
+      }
+      catch { }
+    },
+    Save: (values) => {
+      try {
+        const jsonString = JSON.stringify(values);
+        const url = new URL(location.href);
+        url.searchParams.set(options.Key, jsonString);
+        history.replaceState({}, '', url);
+      }
+      catch { }
+    },
+  };
+});
